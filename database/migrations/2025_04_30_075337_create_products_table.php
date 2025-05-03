753ce99e289fca05fe2f82aa8dff57c5f4931d76
rioanggoro <rioanggoro37@gmail.com>
2025-05-03 16:50:17 +0700

    fix:bug ketika di delete categori product maka di add product otomatis ke delete

--- database/migrations/2025_04_30_075337_create_products_table.php
+++ database/migrations/2025_04_30_075337_create_products_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->unsignedBigInteger('category_id')->nullable();
             $table->string('name');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->decimal('price', 12, 2)->nullable();
             $table->unsignedInteger('preorder')->nullable()->default(0);
             $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->nullOnDelete();
         });
     }
 
