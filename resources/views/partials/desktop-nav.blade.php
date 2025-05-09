76fee65cc39ff0006ad8a95a6d14fc8b61d71513
rioanggoro <rioanggoro37@gmail.com>
2025-05-10 02:51:43 +0700

    fix: in mobile navbar

--- resources/views/partials/desktop-nav.blade.php
+++ resources/views/partials/desktop-nav.blade.php
@@ -2,8 +2,6 @@
     <nav class="hidden md:flex justify-center gap-10 text-sm font-semibold py-4 border-b">
         <a href="{{ route('shop.index') }}" class="hover:text-green-700">Produk</a>
         <a href="{{ route('about') }}" class="hover:text-green-700">Profil</a>
-
         <a href="{{ route('galery') }}" class="hover:text-green-700">Galeri</a>
         <a href="{{ route('my-store') }}" class="hover:text-green-700">Toko Kami</a>
-
     </nav>
--- resources/views/partials/mobile-nav.blade.php
+++ resources/views/partials/mobile-nav.blade.php
@@ -1,8 +1,14 @@
 <div class="md:hidden border-b">
+    <!-- Tombol menu -->
     <button id="mobileMenuButton" class="flex items-center justify-between w-full py-3 px-4 text-sm font-semibold">
         <span>Menu</span>
-        <span class="material-icons">menu</span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+            stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
     </button>
+
+
 
     <!-- Sidebar menu -->
     <div id="mobileSidebar"
@@ -10,13 +16,18 @@
         <div class="flex items-center justify-between px-4 py-3 border-b">
             <span class="text-lg font-semibold">Menu</span>
             <button id="closeSidebar" class="text-gray-600">
-                <span class="material-icons">close</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-x-icon lucide-x">
+                    <path d="M18 6 6 18" />
+                    <path d="m6 6 12 12" />
+                </svg>
             </button>
         </div>
         <div class="flex flex-col text-sm font-semibold divide-y">
             <a href="{{ route('shop.index') }}" class="py-3 px-4 hover:bg-gray-100">Produk</a>
             <a href="{{ route('about') }}" class="py-3 px-4 hover:bg-gray-100">Profil</a>
-            <a href="#" class="py-3 px-4 hover:bg-gray-100">Galeri</a>
+            <a href="{{ route('galery') }}" class="py-3 px-4 hover:bg-gray-100">Galeri</a>
             <a href="{{ route('my-store') }}" class="py-3 px-4 hover:bg-gray-100">Toko Kami</a>
         </div>
     </div>
